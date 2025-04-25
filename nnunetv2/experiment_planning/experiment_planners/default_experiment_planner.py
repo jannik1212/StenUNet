5fab1c3a79792f6e61cd4f617bb38deaaad28a72
jannik1212 <jannik.bundeli@stud.hslu.ch>
2025-04-25 16:42:28 +0200

    ResNetUnet

--- nnunetv2/experiment_planning/experiment_planners/default_experiment_planner.py
+++ nnunetv2/experiment_planning/experiment_planners/default_experiment_planner.py
@@ -7,7 +7,7 @@ import numpy as np
 from batchgenerators.utilities.file_and_folder_operations import load_json, join, save_json, isfile, maybe_mkdir_p
 from nnunetv2.dynamic_network_architectures.architectures.unet import PlainConvUNet, ResidualEncoderUNet
 from nnunetv2.dynamic_network_architectures.architectures.unet_attention import AttentionUNet
-from nnunetv2.dynamic_network_architectures.architectures.resnet import ResNet200_MedicalNet
+from nnunetv2.dynamic_network_architectures.architectures.resnet200_unet import ResNet200UNet
 from nnunetv2.dynamic_network_architectures.building_blocks.helper import convert_dim_to_conv_op, get_matching_instancenorm
 
 from nnunetv2.configuration import ANISO_THRESHOLD
@@ -47,7 +47,7 @@ class ExperimentPlanner(object):
         self.anisotropy_threshold = ANISO_THRESHOLD
 
         self.UNet_base_num_features = 32
-        self.UNet_class = ResidualEncoderUNet # ResidualEncoderUNet, AttentionUNet, PlainConvUNet, StenUNetPretrained
+        self.UNet_class = ResNet200UNet # ResidualEncoderUNet, AttentionUNet, PlainConvUNet, ResNet200UNet
         # the following two numbers are really arbitrary and were set to reproduce nnU-Net v1's configurations as
         # much as possible
         self.UNet_reference_val_3d = 560000000  # 455600128  550000000
--- nnunetv2/utilities/get_network_from_plans.py
+++ nnunetv2/utilities/get_network_from_plans.py
@@ -4,7 +4,7 @@ from nnunetv2.dynamic_network_architectures.initialization.weight_init import in
 from nnunetv2.utilities.network_initialization import InitWeights_He
 from nnunetv2.utilities.plans_handling.plans_handler import ConfigurationManager, PlansManager
 from nnunetv2.dynamic_network_architectures.architectures.unet_attention import AttentionUNet
-from nnunetv2.dynamic_network_architectures.architectures.medical_net_hf_encoder import StenUNetPretrained
+from nnunetv2.dynamic_network_architectures.architectures.resnet200_unet import ResNet200UNet
 from torch import nn
 
 
@@ -31,7 +31,7 @@ def get_network_from_plans(plans_manager: PlansManager,
         'PlainConvUNet': PlainConvUNet,
         'ResidualEncoderUNet': ResidualEncoderUNet,
         'AttentionUNet': AttentionUNet,
-        'StenUNetPretrained': StenUNetPretrained
+        'ResNet200UNet': ResNet200UNet
     }
     kwargs = {
         'PlainConvUNet': {
@@ -57,7 +57,16 @@ def get_network_from_plans(plans_manager: PlansManager,
             'nonlin': nn.LeakyReLU,
             'nonlin_kwargs': {'inplace': True},
         },
-        'StenUNetPretrained': {}
+        'ResNet200UNet': {
+            'conv_bias': True,
+            'norm_op': get_matching_instancenorm(conv_op),
+            'norm_op_kwargs': {'eps': 1e-5, 'affine': True},
+            'dropout_op': None,
+            'dropout_op_kwargs': None,
+            'nonlin': nn.LeakyReLU,
+            'nonlin_kwargs': {'inplace': True},
+        }
+
     }
 
     assert segmentation_network_class_name in mapping.keys(), 'The network architecture specified by the plans file ' \
@@ -72,34 +81,22 @@ def get_network_from_plans(plans_manager: PlansManager,
         if network_class != ResidualEncoderUNet else 'n_blocks_per_stage': configuration_manager.n_conv_per_stage_encoder,
         'n_conv_per_stage_decoder': configuration_manager.n_conv_per_stage_decoder
     }
-    # network class name!!
-    if segmentation_network_class_name == 'StenUNetPretrained':
-        model = StenUNetPretrained(
-            num_input_channels=num_input_channels,
-            n_stages=num_stages,
-            features_per_stage=[min(configuration_manager.UNet_base_num_features * 2 ** i,
-                                    configuration_manager.unet_max_num_features) for i in range(num_stages)],
-            conv_op=conv_op,
-            kernel_size=configuration_manager.conv_kernel_sizes[0],  # just pick one, doesn't affect pretrained encoder
-            strides=configuration_manager.pool_op_kernel_sizes,
-            blocks_per_stage_encoder=configuration_manager.n_conv_per_stage_encoder,
-            blocks_per_stage_decoder=configuration_manager.n_conv_per_stage_decoder,
-            num_classes=label_manager.num_segmentation_heads,
-        )
-    else:
-        model = network_class(
-            input_channels=num_input_channels,
-            n_stages=num_stages,
-            features_per_stage=[min(configuration_manager.UNet_base_num_features * 2 ** i,
-                                    configuration_manager.unet_max_num_features) for i in range(num_stages)],
-            conv_op=conv_op,
-            kernel_sizes=configuration_manager.conv_kernel_sizes,
-            strides=configuration_manager.pool_op_kernel_sizes,
-            num_classes=label_manager.num_segmentation_heads,
-            deep_supervision=deep_supervision,
-            **conv_or_blocks_per_stage,
-            **kwargs[segmentation_network_class_name]
-        )
+    network_class = mapping[segmentation_network_class_name]
+
+    model = network_class(
+        input_channels=num_input_channels,
+        n_stages=num_stages,
+        features_per_stage=[min(configuration_manager.UNet_base_num_features * 2 ** i,
+                                configuration_manager.unet_max_num_features) for i in range(num_stages)],
+        conv_op=conv_op,
+        kernel_sizes=configuration_manager.conv_kernel_sizes,
+        strides=configuration_manager.pool_op_kernel_sizes,
+        num_classes=label_manager.num_segmentation_heads,
+        deep_supervision=deep_supervision,
+        **conv_or_blocks_per_stage,
+        **kwargs[segmentation_network_class_name]
+    )
+
     model.apply(InitWeights_He(1e-2))
     if network_class == ResidualEncoderUNet:
         model.apply(init_last_bn_before_add_to_0)
